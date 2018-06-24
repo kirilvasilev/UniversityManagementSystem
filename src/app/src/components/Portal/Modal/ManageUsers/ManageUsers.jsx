16a611ec3faade6c0bc128e743997eb22f9884c7
kirilvasilev <kiril.v92@gmail.com>
2018-06-24 17:13:24 +0300

    User management changed to table

--- src/app/src/components/Portal/Modal/ManageUsers/ManageUsers.jsx
+++ src/app/src/components/Portal/Modal/ManageUsers/ManageUsers.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./ManageUsers.css";
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 
@@ -13,21 +14,55 @@ export class ManageUsers extends React.Component {
         return (
             <React.Fragment>
             {
-                (this.props.users && this.props.users.length > 0)
-                ? this.props.users.map(user => <h1 key={(Math.random() * 10000)}>
-                        {user.firstName} {user.lastName} {user.isLecturer ? <span onClick={() => {
-                            this.updateUser(user.id, 0)
-                        }}>Make student</span> : <span onClick={() => {
-                            this.updateUser(user.id, 1)
-                        }}>Make lecturer</span>}
-                        <button className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" onClick={() => {
-                                this.deleteUser(user.id)
-                            }}>
-                                <i className="material-icons">delete</i>
-                            </button>
-                        </h1>
-                    )
-                : <h1>test</h1>
+                <table className="mdl-data-table mdl-js-data-table mdl-shadow--2dp" id="users-data">
+                <thead>
+                <tr>
+                    <th className="mdl-data-table__cell">No.</th>
+                    <th className="mdl-data-table__cell--non-numeric">User name</th>
+                    <th className="mdl-data-table__cell--non-numeric">Lecturer</th>
+                    <th className="mdl-data-table__cell--non-numeric"></th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                
+                (this.props.users && this.props.users.length > 0) ?
+                this.props.users.sort((a, b) => {
+                    let nameA = a.firstName.toUpperCase(); // ignore upper and lowercase
+                    let nameB = b.firstName.toUpperCase(); // ignore upper and lowercase
+                    if (nameA < nameB) {
+                      return -1;
+                    }
+                    if (nameA > nameB) {
+                      return 1;
+                    }
+                  
+                    // names must be equal
+                    return 0;
+                  }).map((user, i) => 
+                <tr key={i}>
+                    <td className="mdl-data-table__cell">{i+1}</td>
+                    <td className="mdl-data-table__cell--non-numeric">{user.firstName} {user.lastName}</td>
+                    <td className="mdl-data-table__cell--non-numeric">
+                    <label className="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" htmlFor="isLecuter">
+                        <input type="checkbox" id="isLecuter" className="mdl-checkbox__input" checked={user.isLecturer} onChange={() => {
+                                 this.updateUser(user.id, user.isLecturer);
+                             }}/>
+                        <span className="mdl-switch__label"></span>
+                    </label>
+                    </td>
+                    <td className="mdl-data-table__cell--non-numeric">
+                        <button className="mdl-button mdl-js-button mdl-button--icon" id="delete-user" onClick={() => {
+                                 this.deleteUser(user.id)
+                             }}>
+                            <i className="material-icons">delete_forever</i>
+                            <div className="mdl-tooltip" data-mdl-for="delete-user">Delete user</div>
+                        </button>
+                    </td>
+                </tr>): null
+                }
+                </tbody>
+                </table>
             }
             </React.Fragment>
 
@@ -39,6 +74,9 @@ export class ManageUsers extends React.Component {
     }
 
     updateUser(id, isLecturer) {
+        
+        let userIndex = this.props.users.findIndex(user => user.id == id);
+        this.props.users[userIndex].isLecturer = !isLecturer;
         this.props.updateUser(id, isLecturer)
     }
 }
